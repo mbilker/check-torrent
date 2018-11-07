b8209cbe295088126350e8d09b2cb43a79ed2f2f
Matt Bilker <me@mbilker.us>
2018-11-07 07:32:49 +0000

    main: check file existance, sizes, and for extra files not in torrent, and more error handling

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
+extern crate indicatif;
 extern crate rayon;
 extern crate serde;
 extern crate serde_bencode;
 extern crate serde_bytes;
 extern crate sha1;
 
+#[macro_use] extern crate failure;
 #[macro_use] extern crate serde_derive;
 
 use std::cmp;
 use std::env;
+use std::ffi::OsStr;
 use std::fmt::{self, Write};
-use std::fs::File as FsFile;
+use std::fs::{self, File as FsFile};
 use std::io::{self, Read, Seek, SeekFrom};
 use std::mem;
+use std::path::Component;
 use std::sync::mpsc;
 
+use failure::{Error, Fallible, ResultExt};
+use indicatif::ProgressBar;
 use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
 use serde_bencode::de;
 use serde_bytes::ByteBuf;
 use sha1::Sha1;
+use walkdir::{DirEntry, WalkDir};
 
 fn to_hex_string(data: &[u8]) -> String {
   data.iter().fold(String::with_capacity(data.len() * 2), |mut cur, new| {
@@ -116,19 +123,22 @@ struct Torrent {
   created_by: Option<String>,
 }
 
-fn compute_piece_hash(files: &[FileIndex]) -> Result<[u8; 20], io::Error> {
+fn compute_piece_hash(files: &[FileIndex]) -> Fallible<[u8; 20]> {
   let mut hasher = Sha1::new();
 
   for FileIndex { path, start, end, .. } in files {
-    //println!("path: {:?} (start: {}, end: {})", path, start, end);
-
     // Offset the read bytes to the start of the part of the file we actually
     // care about
     let mut read_counter = *start;
 
-    let mut fd = FsFile::open(path.join("/"))?;
+    let path = path.join("/");
+    let mut fd = FsFile::open(&path).with_context(|_| {
+      format!("Failed to open '{}'", path)
+    })?;
     if read_counter > 0 {
-      fd.seek(SeekFrom::Start(read_counter as u64))?;
+      fd.seek(SeekFrom::Start(read_counter as u64)).with_context(|_| {
+        format!("Failed to seek to {} in '{}'", read_counter, path)
+      })?;
     }
 
     const BUFFER_LENGTH: usize = 64 * 1024;
@@ -136,15 +146,15 @@ fn compute_piece_hash(files: &[FileIndex]) -> Result<[u8; 20], io::Error> {
 
     loop {
       let request_amount = cmp::min(BUFFER_LENGTH, *end - read_counter);
-      let read_amount = fd.read(&mut buffer[..request_amount])?;
-      //println!("read (num: {:?}, read_bytes: {:?})", num, read_bytes);
+      let read_amount = fd.read(&mut buffer[..request_amount]).with_context(|_| {
+        format!("Failed to read from '{}'", path)
+      })?;
 
       if read_amount == 0 {
         break;
       }
 
       if read_counter + read_amount >= *end {
-        //println!("hit end (read_bytes: {}, buffer len: {}, read_bytes + buffer len = {})", read_bytes, buffer.len(), read_bytes + buffer.len());
         let remaining = end - read_counter;
         hasher.update(&buffer[..remaining]);
 
@@ -165,7 +175,253 @@ fn compute_piece_hash(files: &[FileIndex]) -> Result<[u8; 20], io::Error> {
   Ok(computed_hash)
 }
 
-fn render_torrent(torrent: &Torrent) -> io::Result<()> {
+fn files_existance_check(files: &[File]) -> Fallible<()> {
+  let mut file_errors = Vec::new();
+
+  for f in files {
+    let path = f.path.join("/");
+    let meta = match fs::metadata(&path) {
+      Ok(v) => v,
+      Err(e) => {
+        if e.kind() == io::ErrorKind::NotFound {
+          file_errors.push(format_err!("'{}' is missing", path));
+          continue;
+        }
+
+        return Err(Error::from_boxed_compat(Box::new(e)).context(
+          format!("Failed to get the metadata of '{}'", path)
+        ).into());
+      },
+    };
+
+    if !meta.is_file() {
+      file_errors.push(format_err!("'{}' is not a file", path));
+      continue;
+    }
+
+    if meta.len() != f.length as u64 {
+      file_errors.push(format_err!("'{}' is not of size {} bytes", path, f.length));
+    }
+  }
+
+  if !file_errors.is_empty() {
+    println!("File Errors:");
+    for err in file_errors {
+      println!("{}", err);
+    }
+    println!();
+
+    Err(format_err!("Missing files or mismatched file sizes"))
+  } else {
+    Ok(())
+  }
+}
+
+fn is_file(entry: &DirEntry) -> bool {
+  entry.metadata().unwrap()
+    .is_file()
+}
+
+fn extra_files_check(files: &[File]) -> Fallible<()> {
+  let walker = WalkDir::new(".")
+    .into_iter()
+    .filter_map(|e| e.ok())
+    .filter(is_file);
+  let extra_files: Vec<_> = walker.filter_map(|entry| {
+    // Ignore the `Component::CurDir` part, only get the path components
+    let components: Vec<_> = entry.path()
+      .components()
+      .filter_map(|component| {
+        match component {
+          Component::Normal(s) => Some(s),
+          _ => None,
+        }
+      })
+      .collect();
+
+    let part_of_torrent = files.iter().any(|file| {
+      let mut torrent_path = file.path.iter()
+        .map(|component| OsStr::new(component));
+      let mut entry_path = components.iter()
+        .map(|component| *component);
+
+      loop {
+        // While similar to using `Iterator::zip`, this checks that both
+        // iterators return `None` at the same time.
+        let torrent_part = torrent_path.next();
+        let entry_part = entry_path.next();
+        if torrent_part != entry_part {
+          return false;
+        }
+
+        // Base case of the loop is when both iterators return `None`
+        if torrent_part == None && entry_part == None {
+          break;
+        }
+      }
+
+      true
+    });
+
+    if part_of_torrent {
+      None
+    } else {
+      Some(entry.path().to_path_buf())
+    }
+  }).collect();
+
+  if !extra_files.is_empty() {
+    println!("Extra Files:");
+    for file in extra_files {
+      println!("{:?}", file.as_path());
+    }
+    println!();
+  }
+
+  Ok(())
+}
+
+fn files_hash_check(files: &[File], piece_size: usize, pieces: &[u8]) -> Fallible<()> {
+  // Each piece is a 160-bit SHA-1 hash
+  let mut pieces = pieces.chunks(20);
+
+  let mut piece_files = Vec::new();
+  let mut current_piece = Vec::new();
+
+  let mut read_bytes = 0;
+
+  for f in files {
+    //println!("file path:\t{:?}", f.path);
+    //println!("file length:\t{}", f.length);
+    //println!("file md5sum:\t{:?}", f.md5sum);
+
+    let file_length = f.length as usize;
+
+    // Check if the file size will fill the current piece
+    if read_bytes + file_length < piece_size {
+      current_piece.push(FileIndex {
+        path: &f.path,
+        start: 0,
+        end: f.length as usize,
+        end_type: PieceFileType::Whole,
+      });
+
+      read_bytes += file_length;
+    } else {
+      let mut start = 0;
+
+      // Determine the number of pieces the current file will occupy,
+      // taking into account the remaining size of the current piece
+      let remaining = piece_size - read_bytes;
+      if remaining > 0 {
+        let piece = pieces.next().unwrap();
+        //println!("read piece (remaining): {:02x?}", piece);
+
+        current_piece.push(FileIndex {
+          path: &f.path,
+          start: 0,
+          end: remaining,
+          end_type: PieceFileType::Partial,
+        });
+
+        start = remaining;
+
+        let files = mem::replace(&mut current_piece, Vec::new());
+        piece_files.push((piece, files));
+      }
+
+      let num_pieces = (file_length - start) / piece_size;
+      for _ in 0..num_pieces {
+        let piece = pieces.next().unwrap();
+        //println!("read piece: {:02x?}", piece);
+
+        let files = vec![FileIndex {
+          path: &f.path,
+          start,
+          end: start + piece_size,
+          end_type: PieceFileType::Partial,
+        }];
+        piece_files.push((piece, files));
+
+        start += piece_size;
+      }
+
+      current_piece.push(FileIndex {
+        path: &f.path,
+        start,
+        end: file_length,
+        end_type: PieceFileType::Partial,
+      });
+      read_bytes = file_length - start;
+    }
+  }
+
+  if let Some(piece) = pieces.next() {
+    let files = mem::replace(&mut current_piece, Vec::new());
+    piece_files.push((piece, files));
+  }
+
+  if pieces.next().is_some() {
+    return Err(format_err!("There should be no more pieces"));
+  }
+
+  let mut all_ok = true;
+  let bar = ProgressBar::new(piece_files.len() as u64);
+  let (sender, receiver) = mpsc::channel();
+
+  bar.enable_steady_tick(10);
+
+  piece_files
+    .par_iter()
+    .enumerate()
+    .try_for_each_with(sender, |s, (i, (piece, files))| -> Fallible<()> {
+      let computed_hash = match compute_piece_hash(files) {
+        Ok(v) => v,
+        Err(e) => {
+          eprintln!("Error processing {:#?}: {:?}", files, e);
+          return Err(e);
+        },
+      };
+
+      bar.inc(1);
+
+      if computed_hash != *piece {
+        bar.set_message(&format!("Hash mismatch! (#{}, digest: {:?}, piece: {:?})",
+          i,
+          to_hex_string(&computed_hash),
+          to_hex_string(piece)));
+
+        s.send((piece, files, computed_hash)).unwrap();
+      } else {
+        bar.set_message(&format!("Hash correct for piece #{} ({})", i, to_hex_string(piece)));
+      }
+      Ok(())
+    })?;
+
+  bar.finish_and_clear();
+
+  for (piece, files, computed_hash) in receiver {
+    eprintln!("Hash mismatch!");
+    eprintln!(" - Files:");
+    for file in files {
+      eprintln!("   - {} (start: {}, end: {}, type: {:?})", file.path.join("/"), file.start, file.end, file.end_type);
+    }
+    eprintln!(" - Torrent piece hash: {}", to_hex_string(piece));
+    eprintln!(" - Computed hash:      {}", to_hex_string(&computed_hash));
+
+    all_ok = false;
+  }
+
+  if all_ok {
+    println!("All pieces are good!");
+  } else {
+    println!("One or more pieces are invalid");
+  }
+
+  Ok(())
+}
+
+fn render_torrent(torrent: &Torrent) -> Fallible<()> {
   println!("name:\t\t{}", torrent.info.name);
   println!("announce:\t{:?}", torrent.announce);
   println!("nodes:\t\t{:?}", torrent.nodes);
@@ -192,151 +448,49 @@ fn render_torrent(torrent: &Torrent) -> io::Result<()> {
   println!("pieces count: {}", count);
 
   if let &Some(ref files) = &torrent.info.files {
+    extra_files_check(files)?;
+    files_existance_check(files)?;
+
     let piece_size = torrent.info.piece_length as usize;
     println!("piece_size: {}", piece_size);
 
-    // Each piece is a 160-bit SHA-1 hash
-    let mut pieces = torrent.info.pieces.chunks(20);
-
-    let mut piece_files = Vec::new();
-    let mut current_piece = Vec::new();
-
-    let mut read_bytes = 0;
-
-    for f in files {
-      //println!("file path:\t{:?}", f.path);
-      //println!("file length:\t{}", f.length);
-      //println!("file md5sum:\t{:?}", f.md5sum);
-
-      let file_length = f.length as usize;
-
-      // Check if the file size will fill the current piece
-      if read_bytes + file_length < piece_size {
-        current_piece.push(FileIndex {
-          path: &f.path,
-          start: 0,
-          end: f.length as usize,
-          end_type: PieceFileType::Whole,
-        });
-
-        read_bytes += file_length;
-      } else {
-        let mut start = 0;
-
-        // Determine the number of pieces the current file will occupy,
-        // taking into account the remaining size of the current piece
-        let remaining = piece_size - read_bytes;
-        if remaining > 0 {
-          let piece = pieces.next().unwrap();
-          //println!("read piece (remaining): {:02x?}", piece);
-
-          current_piece.push(FileIndex {
-            path: &f.path,
-            start: 0,
-            end: remaining,
-            end_type: PieceFileType::Partial,
-          });
-
-          start = remaining;
-
-          let files = mem::replace(&mut current_piece, Vec::new());
-          piece_files.push((piece, files));
-        }
-
-        let num_pieces = (file_length - start) / piece_size;
-        for _ in 0..num_pieces {
-          let piece = pieces.next().unwrap();
-          //println!("read piece: {:02x?}", piece);
-
-          let files = vec![FileIndex {
-            path: &f.path,
-            start,
-            end: start + piece_size,
-            end_type: PieceFileType::Partial,
-          }];
-          piece_files.push((piece, files));
-
-          start += piece_size;
-        }
-
-        current_piece.push(FileIndex {
-          path: &f.path,
-          start,
-          end: file_length,
-          end_type: PieceFileType::Partial,
-        });
-        read_bytes = file_length - start;
-      }
-    }
-
-    if let Some(piece) = pieces.next() {
-      let files = mem::replace(&mut current_piece, Vec::new());
-      piece_files.push((piece, files));
-    }
-
-    assert!(!pieces.next().is_some(), "there should be no more pieces");
-
-    let mut all_ok = true;
-    let (sender, receiver) = mpsc::channel();
-
-    piece_files
-      .par_iter()
-      .enumerate()
-      .try_for_each_with(sender, |s, (i, (piece, files))| -> Result<(), io::Error> {
-        let computed_hash = match compute_piece_hash(files) {
-          Ok(v) => v,
-          Err(e) => {
-            eprintln!("Error processing {:#?}: {:?}", files, e);
-            return Err(e);
-          },
-        };
-        if computed_hash != *piece {
-          println!("Hash mismatch! (#{}, digest: {:?}, piece: {:?})",
-            i,
-            to_hex_string(&computed_hash),
-            to_hex_string(piece));
-
-          s.send((piece, files, computed_hash)).unwrap();
-        } else {
-          println!("Hash correct for piece #{} ({})", i, to_hex_string(piece));
-        }
-        Ok(())
-      })
-      .unwrap();
-
-    for (piece, files, computed_hash) in receiver {
-      eprintln!("Hash mismatch!");
-      eprintln!(" - Files:");
-      for file in files {
-        eprintln!("   - {} (start: {}, end: {}, type: {:?})", file.path.join("/"), file.start, file.end, file.end_type);
-      }
-      eprintln!(" - Torrent piece hash: {}", to_hex_string(piece));
-      eprintln!(" - Computed hash:      {}", to_hex_string(&computed_hash));
-
-      all_ok = false;
-    }
-
-    if all_ok {
-      println!("All pieces are good!");
-    } else {
-      println!("One or more pieces are invalid");
-    }
+    files_hash_check(files, piece_size, &torrent.info.pieces)?;
   }
 
   Ok(())
 }
 
-fn main() -> io::Result<()> {
-  if let Some(arg1) = env::args().nth(1) {
-    let mut buffer = Vec::new();
-    let mut fd = FsFile::open(arg1)?;
-    fd.read_to_end(&mut buffer)?;
+fn check_torrent_file(path: &str) -> Fallible<()> {
+  let mut buffer = Vec::new();
+  let mut fd = FsFile::open(path)?;
+  fd.read_to_end(&mut buffer)?;
 
-    match de::from_bytes::<Torrent>(&buffer) {
-      Ok(t) => render_torrent(&t)?,
-      Err(e) => eprintln!("Error parsing torrent file: {:?}", e),
-    };
-  }
+  match de::from_bytes::<Torrent>(&buffer) {
+    Ok(t) => render_torrent(&t)?,
+    Err(e) => eprintln!("Error parsing torrent file: {:?}", e),
+  };
 
   Ok(())
+}
+
+fn main() {
+  if let Some(arg1) = env::args().nth(1) {
+    if let Err(e) = check_torrent_file(&arg1) {
+      eprintln!("An error occurred while checking {}:", arg1);
+      eprintln!("  {}", e);
+      eprintln!();
+
+      for cause in e.iter_causes() {
+        eprintln!("Caused by: {}", cause);
+      }
+      if e.as_fail().cause().is_some() {
+        eprintln!();
+      }
+
+      if let Some(backtrace) = e.as_fail().backtrace() {
+        eprintln!("Backtrace:");
+        eprintln!("{}", backtrace);
+      }
+    }
+  }
 }
